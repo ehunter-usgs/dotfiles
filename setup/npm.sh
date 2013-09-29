c4a24e22a0f3952083f21dbf1515c1e60579b996
Eric Martinez <emartinez@usgs.gov>
2013-09-29 12:30:38 -0600

    Added initial npm installs.

---
+++ setup/npm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# As of Yeoman 1.0, this gets bower and grunt-cli as well
+npm install -g yo
+
+npm install -g jshint
+npm install -g jake
+
+npm install -g generator-webapp
+npm install -g generator-generator
+npm install -g generator-hazdev-webapp
+npm install -g generator-hazdev-weblib
+
+exit 0;
