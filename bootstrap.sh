4c972adffc1bd7ba396c1c576d57fdc49098a105
Eric Martinez <emartinez@usgs.gov>
2013-09-29 14:24:40 -0600

    Added post-bootstrap install scripts.

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,17 +12,36 @@ function doIt() {
 		--exclude "README.md" \
 		--exclude "setup" \
 		--exclude "themes" \
-		-av --no-perms . ~
+		-av --no-perms --dry-run . ~
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	read -p "This may overwrite existing files. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
 	fi
 fi
+
+read -p "Install packages etc...? (y/n)" -n 1 REPLY
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo 'setup/brew.sh';
+fi
+
+read -p "Install extras? (y/n)" -n 1 REPLY
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo 'setup/extra.sh';
+fi
+
+read -p "Run Mountain Lion customization? (y/n)" -n 1 REPLY
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo 'setup/osx-mlion.sh';
+fi
+
 unset doIt
 source ~/.bash_profile
